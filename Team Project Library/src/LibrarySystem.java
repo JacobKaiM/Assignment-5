c9c23e4a023887e7ddb94e8de7754f2e7cfd77d2
humanityguys <humanityguys@gmail.com>
2024-06-09 19:46:33 -0700

    Update LibrarySystem.java
    added several new books

--- Team Project Library/src/LibrarySystem.java
+++ Team Project Library/src/LibrarySystem.java
@@ -3,9 +3,30 @@ import java.util.Scanner;
 public class LibrarySystem {
     public static void main(String[] args) {
     	 Library library = new Library();
-         library.addBook(new Book("Player's HandBook", "Gary Gygax", "Fantasy"));
+          //Roleplaying Books
+         library.addBook(new Book("Players Handbook", "Gary Gygax", "RolePlaying"));
+         library.addBook(new Book("The Monster Manual","Gary Gygax","Roleplaying"));
+         library.addBook(new Book("Dungeon Masters Guide","Gary Gygax","Roleplaying"));
+         //Horror Section
          library.addBook(new Book("House of Leaves", "Mark Z. Danielewski", "Horror"));
+         library.addBook(new Book("The Shining","Stephen King","Horror"));
+         library.addBook(new Book("Dracula","Bram Stoker","Horror"));
+         //Manga
          library.addBook(new Book("Haikyuu!", "Haruichi Furuda", "Manga"));
+         library.addBook(new Book("Fullmetal Alchemist","Hiromu Arakawa","Manga"));
+         library.addBook(new Book("One Piece","Eiichiro Oda","Manga"));
+         //Sci-Fi
+         library.addBook(new Book("Hyperion","Dan Simmons","Science Fiction"));
+         library.addBook(new Book("The Hitchhiker's Guide to the Galaxy","Douglas Adams","Science Fiction"));
+         library.addBook(new Book("Neuromancer","William Gibson","Science Fiction"));
+         //Classics
+         library.addBook(new Book("The Count of Monte Cristo","Alexandre Dumas","Classic"));
+         library.addBook(new Book("Moby Dick","Herman Melville","Classic"));
+         library.addBook(new Book("Farenheit 451","Ray Bradbury","Classic"));
+         //Fantasy
+         library.addBook(new Book("The Last Wish","Andrzej Sapkowski", "Fantasy"));
+         library.addBook(new Book("The Lord of the Rings","J.R.R. Tolkien", "Fantasy"));
+         library.addBook(new Book("Howl's Moving Castle","Diana Wynne Jones", "Fantasy"));
 
 
         Scanner scanner = new Scanner(System.in);
