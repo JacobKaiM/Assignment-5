2cc412e1cf54857ab9948c13fb66d02170c18f6a
humanityguys <humanityguys@gmail.com>
2024-06-13 15:36:34 -0700

    Update Library.java

--- Team Project Library/src/Library.java
+++ Team Project Library/src/Library.java
@@ -1,58 +1,68 @@
 import java.util.ArrayList;
 
 class Library {
-    private ArrayList<Book> books;
+	private ArrayList<Book> books;
 
-    public Library() {
-        this.books = new ArrayList<>();
-    }
+	public Library() {
+		this.books = new ArrayList<>(18);
+	}
 
-    public void addBook(Book book) {
-        books.add(book);
-    }
+	public void addBook(Book book) {
+			books.add(book);
+	}
 
-    public void displayAvailableBooks() {
-        System.out.println("Available Books:");
-        for (Book book : books) {
-            if (!book.isCheckedOut()) {
-                System.out.println("Title: " + book.getTitle() + ", Author: " + book.getAuthor() + ", Genre: " + book.getGenre());
-            }
-        }
-    }
+	public void displayGenres(String genre) {
+		if (genre.equalsIgnoreCase("Roleplaying")) {
+			displayAvailableBooks(1);
+		} else if (genre.equalsIgnoreCase("Horror")) {
+			displayAvailableBooks(2);
+		} else if (genre.equalsIgnoreCase("Manga")) {
+			displayAvailableBooks(3);
+		} else if (genre.equalsIgnoreCase("Science Fiction")) {
+			displayAvailableBooks(4);
+		} else if (genre.equalsIgnoreCase("Classics")) {
+			displayAvailableBooks(5);
+		} else if (genre.equalsIgnoreCase("Fantasy")) {
+			displayAvailableBooks(6);
+		}
+	}
 
-public void checkoutBook(String title) {
-        boolean found = false;
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(title) && !book.isCheckedOut()) {
-                book.setCheckedOut(true);
-                System.out.println("Thank you for checking out \"" + title + "\". Enjoy!");
-                found = true;
-                break;
-            }
-            if(book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
-            	System.out.println(title + " is currently checked out.");
-            	break;
-            }
-            else {
-            	String excep = "Sorry, \"" + title + "\" is not in this library.";
-                throw new IllegalArgumentException(excep);
-            }
-        }
-    }
+	public void displayAvailableBooks(int choice) {
+		System.out.println("Available Books:");
+		for (Book book : books) {
+			if (!book.isCheckedOut() && book.getGenre() == choice) {
+				System.out.println("Title: " + book.getTitle() + ", Author: " + book.getAuthor());
+			}
+		}
+	}
 
-      public void returnBook(String title) {
-        boolean found = false;
-        for (Book book : books) {
-            if (book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
-                book.setCheckedOut(false);
-                System.out.println("Thank you for returning \"" + title + "\".");
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-        	String excep = "Sorry, \"" + title + "\" cannot be returned.";
-           	throw new IllegalArgumentException(excep);
-        }
-    }
+	public void checkoutBook(String title) {
+		for (Book book : books) {
+			if (book.getTitle().equalsIgnoreCase(title) && !book.isCheckedOut()) {
+				book.setCheckedOut(true);
+				System.out.println("Thank you for checking out \"" + title + "\". Enjoy!");
+				break;
+			} 
+			if (book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
+				System.out.println(title + " is currently checked out.");
+				break;
+			}
+		}
+	}
+
+	public void returnBook(String title) {
+		boolean found = false;
+		for (Book book : books) {
+			if (book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
+				book.setCheckedOut(false);
+				System.out.println("Thank you for returning \"" + title + "\".");
+				found = true;
+				break;
+			}
+		}
+		if (!found) {
+			String excep = "Sorry, \"" + title + "\" cannot be returned.";
+			throw new IllegalArgumentException(excep);
+		}
+	}
 }
