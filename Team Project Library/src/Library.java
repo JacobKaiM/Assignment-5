5b4e0eae691af16a97e7b8a7632ca75e7a0e3d44
humanityguys <humanityguys@gmail.com>
2024-06-13 16:38:20 -0700

    Update Library.java

--- Team Project Library/src/Library.java
+++ Team Project Library/src/Library.java
@@ -24,7 +24,7 @@ class Library {
 			displayAvailableBooks(5);
 		} else if (genre.equalsIgnoreCase("Fantasy")) {
 			displayAvailableBooks(6);
-		}else if (genre.equalsIgnoreCase("Philosophy"){
+		}else if (genre.equalsIgnoreCase("Philosophy")) {
 			displayAvailableBooks(7);
 		}
 	}
