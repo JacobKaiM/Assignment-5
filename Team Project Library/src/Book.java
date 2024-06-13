13d91d9736c5230c1594ee633e07f0cdba6effe3
humanityguys <humanityguys@gmail.com>
2024-06-13 15:35:50 -0700

    Update Book.java
    allowed for further code changes

--- Team Project Library/src/Book.java
+++ Team Project Library/src/Book.java
@@ -1,34 +1,42 @@
 
 class Book {
-    private String title;
-    private String author;
-    private String genre;
-    private boolean checkedOut;
+	private String title;
+	private String author;
+	private int genre;
+	private boolean checkedOut;
 
-    public Book(String title, String author, String genre) {
-        this.title = title;
-        this.author = author;
-        this.genre = genre;
-        this.checkedOut = false;
-    }
+	public Book(String title, String author, int genre) {
+		this.title = title;
+		this.author = author;
+		this.genre = genre;
+		this.checkedOut = false;
+	}
 
-    public String getTitle() {
-        return title;
-    }
+	public String getTitle() {
+		return title;
+	}
 
-    public String getAuthor() {
-        return author;
-    }
+	public String getAuthor() {
+		return author;
+	}
 
-    public String getGenre() {
-        return genre;
-    }
+	public int getGenre() {
+		return genre;
+	}
 
-    public boolean isCheckedOut() {
-        return checkedOut;
-    }
+	public boolean isCheckedOut() {
+		return checkedOut;
+	}
 
-    public void setCheckedOut(boolean checkedOut) {
-        this.checkedOut = checkedOut;
-    }
+	public void setCheckedOut(boolean checkedOut) {
+		this.checkedOut = checkedOut;
+	}
+
+	public boolean contains(int choice) {
+		if (choice == genre) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
