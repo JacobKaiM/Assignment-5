1f3b8b51f6cedb25fccb52b7eab01754dd4489f7
JacobKaiM <155326506+JacobKaiM@users.noreply.github.com>
2024-06-13 18:50:19 -0700

    Update Library.java

--- Team Project Library/src/Library.java
+++ Team Project Library/src/Library.java
@@ -1,12 +1,18 @@
 import java.util.ArrayList;
-
+/*
+ * Library class represents the library itself 
+ * used to keep track of A books genre and availability 
+ * also when a book is checked out and returned.
+ */
 class Library {
 	private ArrayList<Book> books;
 
+	// initialize library ArrayList
 	public Library() {
 		this.books = new ArrayList<>(18);
 	}
-
+	
+	// adds book to the list 
 	public void addBook(Book book) {
 			books.add(book);
 	}
@@ -37,14 +43,19 @@ class Library {
 			}
 		}
 	}
-
+	
+	
 	public void checkoutBook(String title) {
+		// loop through books in list
 		for (Book book : books) {
+			// if title matches and is not checked out 
+			// set Checked out to true 
 			if (book.getTitle().equalsIgnoreCase(title) && !book.isCheckedOut()) {
 				book.setCheckedOut(true);
 				System.out.println("Thank you for checking out \"" + title + "\". Enjoy!");
 				break;
 			} 
+			// if title matched but is already checked out tell user they cant check it out
 			if (book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
 				System.out.println(title + " is currently checked out.");
 				break;
@@ -53,8 +64,12 @@ class Library {
 	}
 
 	public void returnBook(String title) {
+		// if book is not found
 		boolean found = false;
+		// loop through books in list 
 		for (Book book : books) {
+			// if book title and is checked out reset checked out to false 
+			// change found to true 
 			if (book.getTitle().equalsIgnoreCase(title) && book.isCheckedOut()) {
 				book.setCheckedOut(false);
 				System.out.println("Thank you for returning \"" + title + "\".");
@@ -62,6 +77,8 @@ class Library {
 				break;
 			}
 		}
+		
+		// if not found error message
 		if (!found) {
 			String excep = "Sorry, \"" + title + "\" cannot be returned.";
 			throw new IllegalArgumentException(excep);
